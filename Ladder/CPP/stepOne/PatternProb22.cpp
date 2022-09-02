6ab17d2ab793bd3476395b4da3af33f6a3738baa
k-nautresurrect <kushignite@gmail.com>
2022-09-02 11:58:05 +0530

    updated prob22

--- Ladder/CPP/stepOne/PatternProb22.cpp
+++ Ladder/CPP/stepOne/PatternProb22.cpp
@@ -20,23 +20,47 @@ int main() {
 	int n; cin >> n;
 	int rows = n;
 	int cols = rows;
+	int middle = 2*n - 3;
 
+	// first half
 	for (int row = 0; row < rows; row++) {
 		int val = n;
-		int count = (2 * n - 1) - (row + 1);
-		val++;
-		while (val != n) {
-			if (count != 0) {
-				cout << val;
-			} else {
-				val--;
-			}
-			count--;
+		for(int col = 0; col <= row; col++){
+			cout << val << " ";
+			val--;
 		}
-		// if (row == 0 || row == rows) {
-		// 	val = n;
-		// 	for (int col = 0; col < 2 * cols - 2; col++)	cout << val;
-		// }
+		val++;
+		for(int col = 0; col < middle; col++){
+			cout << val << " ";
+		}
+
+		for(int col = 0; col <= row; col++){
+			if(val != 1)	cout << val << " ";
+			val++;
+		}
+
+		middle -= 2;
+		cout << endl;
+	}
+
+	// second half
+	middle = 1;
+	for(int row = 0; row < rows-1; row++){
+		int val = n;
+		for(int col = 0; col < (rows-row) - 1; col++){
+			cout << val << " ";
+			val--;
+		}
+		val++;
+		for(int col = 0; col < middle; col++){
+			cout << val << " ";
+		}
+		for(int col = 0; col < (rows-row)-1; col++){
+			cout << val << " ";
+			val++;
+		}
+
+		middle += 2;
 		cout << endl;
 	}
 }
